5d15324396609ed0e925ee08928de04bf3dd95bc
Kamali <240143.it@rmkec.ac.in>
2026-07-03 15:49:50 +0530

    update reset error

--- task-manager-backend/server.js
+++ task-manager-backend/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://taskora-cnqyszv5y-kamalikesavan8s-projects.vercel.app'
+    'https://taskora-cm2xy7s61-kamalikesavan8s-projects.vercel.app'
   ],
   credentials: true
 }));
